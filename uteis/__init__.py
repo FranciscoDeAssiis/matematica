c577323b5e7811e3f22fa9cd1b424da624791241
Francisco de Assis <rarxn@gmail.com>
2023-11-01 19:35:04 -0300

    Função 'input_number() que converte entrada input em int ou float.

---
+++ uteis/__init__.py
@@ -0,0 +1,47 @@
+class Uteis:
+
+    @staticmethod
+    def input_number(txt='Digite: ', style='blue', format_number=0) -> [int, float]:
+        style = style.upper()
+        if style == 'RED':
+            id_color = 31
+        elif style == 'GREEN':
+            id_color = 32
+        elif style == 'YELLOW':
+            id_color = 33
+        elif style == 'BLUE':
+            id_color = 34
+        elif style == 'MAGENTA':
+            id_color = 35
+        elif style == 'CYAN':
+            id_color = 36
+        else:
+            id_color = 34  # default: blue
+
+        result = None
+        while not isinstance(result, int) or not isinstance(result, float):
+            result = input(f'\33[1;{id_color}m{txt}\33[m')
+
+            if '.' in result:  # flutuante
+                try:
+                    result = float(result)
+                    if format_number:
+                        result = f'{result:,.{format_number}f}'
+                    break
+                except ValueError:
+                    pass
+
+            else:  # inteiro
+                try:
+                    result = int(result)
+                    break
+                except ValueError:
+                    pass
+
+        return result
+
+
+if __name__ == '__main__':
+    uteis = Uteis()
+    resposta = uteis.input_number(format_number=2)
+    print(resposta)

76bc109b31a0159a44e410741df7c79424ae1b23
Francisco <147069814+FranciscoDeAssiis@users.noreply.github.com>
2023-11-09 19:36:16 -0300

    Update expressões numéricas.py

--- basica/expressões numéricas.py
+++ basica/expressões numéricas.py
@@ -1,16 +1,22 @@
-# 7 + {20 + [30 - (11 + 5) + 2]}
-# 7 * {25 - [(12 + 9) - 14] + 3 + 0}
-# 1453 - { 6 * (9 + 4) + [50 * 24 + 3] - 80} - 6
-
-# Prioridade de Símbolos:: 
-#     1º parênteses ()
-#     2º colchetes []
-#     3º chaves {}
-
-# Prioridade nas Operações:
-#     1º potências ou raízes
-#     2º multiplicação ou divisão
-#     3º adição ou subtração
+#########################################################
+# Expressões Numéricas
+#
+#     Prioridade de Símbolos:
+#         1º parênteses ()
+#         2º colchetes []
+#         3º chaves {}
+#
+#     Prioridade nas Operações:
+#         1º potências ou raízes
+#         2º multiplicação ou divisão
+#         3º adição ou subtração
+#
+#     Exemplos de Expressões:
+#         7 + {20 + [30 - (11 + 5) + 2]}
+#         7 * {25 - [(12 + 9) - 14] + 3 + 0}
+#         1453 - { 6 * (9 + 4) + [50 * 24 + 3] - 80} - 6
+# 
+#########################################################
 
 
 def expressão_numérica(exp: str):
